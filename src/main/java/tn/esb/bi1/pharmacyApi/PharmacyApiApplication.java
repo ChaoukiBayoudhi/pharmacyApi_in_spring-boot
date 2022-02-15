2178812aebb8bd324cec9c05e20f05c86e36fcea
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2022-02-15 12:38:31 +0100

    Initial commit

---
+++ src/main/java/tn/esb/bi1/pharmacyApi/PharmacyApiApplication.java
@@ -0,0 +1,13 @@
+package tn.esb.bi1.pharmacyApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/esb/bi1/pharmacyApi/PharmacyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esb.bi1.pharmacyApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
