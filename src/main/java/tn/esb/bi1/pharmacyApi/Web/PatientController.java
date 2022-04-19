593ef0edc5ff6bb9de18f61b6191e6c1d2646f90
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2022-04-19 10:47:27 +0100

    implement Patient Service and Rest Controller

--- src/main/java/tn/esb/bi1/pharmacyApi/Web/PatientController.java
+++ src/main/java/tn/esb/bi1/pharmacyApi/Web/PatientController.java
@@ -28,7 +28,8 @@ public class PatientController {
     {
         return patientServ.addPatient(patient);
     }
-    
+
+
     @GetMapping("/{numss}")
     public Optional<Patient> getPatient(@PathVariable Long numss)
     {
